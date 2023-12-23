7d31f48d1175e7e314fe400c38a5172a444f4a69
Pouya <p.mohammadkhani95@gmail.com>
2023-12-24 00:12:30 +0330

    Add more detail to code

--- python-srt-generator/generate_audio.py
+++ python-srt-generator/generate_audio.py
@@ -8,13 +8,14 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+
 def generate_audio(number, word, raw_text_file_path):
-    # Your API key
+    # TODO: Your API key goes here (create a .env file and add OPENAI_API_KEY=@#$%)
     API_KEY = os.getenv("OPENAI_API_KEY")
 
     file_name = f"{number}.{word}"
     audio_output_name = f"{file_name}.mp3"
-    
+
     desktop_path = Path.home() / "Desktop"
     blulexi_content_folder = desktop_path / "blulexi-content"
 
@@ -24,7 +25,7 @@ def generate_audio(number, word, raw_text_file_path):
     audio_file_path_absolute = blulexi_content_folder / audio_output_name
 
     # Read raw text from the file
-    with open(raw_text_file_path, 'r') as file:
+    with open(raw_text_file_path, "r") as file:
         raw_text = file.read()
 
     client = OpenAI(api_key=API_KEY)
@@ -38,13 +39,20 @@ def generate_audio(number, word, raw_text_file_path):
 
     response.stream_to_file(str(audio_file_path_absolute))
 
-    print(f"Audio file generated and saved to {audio_file_path_absolute}.")
+    print(f"[System]: Audio file generated and saved to {audio_file_path_absolute}.")
+
 
 if __name__ == "__main__":
     # User input
+    print(
+        "[System]: Hello, We are going to make a audio file based on the script you provide.\nGive a 'number' and 'word' to create a file with these two!"
+    )
+    print("[System]: Sample output -> [number].[word].mp3")
     number = input("Enter the number: ")
     word = input("Enter the word: ")
 
-    raw_text_file_path = input("Enter the path to the file containing raw text: ")
+    raw_text_file_path = input(
+        "Enter the path to the file containing raw text(script): "
+    )
 
     generate_audio(number=number, word=word, raw_text_file_path=raw_text_file_path)
--- python-srt-generator/generate_srt.py
+++ python-srt-generator/generate_srt.py
@@ -1,4 +1,4 @@
-# Note: This script is designed to be run in a Jupyter notebook on Google Colab due to 
+# Note: This script is designed to be run in a Jupyter notebook on Google Colab due to
 # issues with the 'aeneas' package installation, particularly on Mac devices.
 # To use this script, execute the following code blocks in a Colab notebook:
 
@@ -20,17 +20,24 @@ from aeneas.executetask import ExecuteTask
 from aeneas.task import Task
 from pathlib import Path
 
-def generate_srt(number, word):
 
+def generate_srt(number, word):
+    # TODO: Feel free to modify these path
     file_name = f"{number}.{word}"
     audio_output_name = f"{file_name}.mp3"
-    audio_file_path_absolute = Path.home() / "Desktop" / "blulexi-content" / audio_output_name
-    text_file_path_absolute = Path.home() / "Desktop" / "blulexi-content" / f"{file_name}-words.txt"
-    sync_map_file_path_absolute = Path.home() / "Desktop" / "blulexi-content" / f"{file_name}.json"
+    audio_file_path_absolute = (
+        Path.home() / "Desktop" / "blulexi-content" / audio_output_name
+    )
+    text_file_path_absolute = (
+        Path.home() / "Desktop" / "blulexi-content" / f"{file_name}-words.txt"
+    )
+    sync_map_file_path_absolute = (
+        Path.home() / "Desktop" / "blulexi-content" / f"{file_name}.json"
+    )
     str_output = Path.home() / "Desktop" / "blulexi-content" / f"{file_name}.srt"
 
     # create Task object
-    config_string = u"task_language=eng|is_text_type=plain|os_task_file_format=json"
+    config_string = "task_language=eng|is_text_type=plain|os_task_file_format=json"
     task = Task(config_string=config_string)
     task.audio_file_path_absolute = str(audio_file_path_absolute)
     task.text_file_path_absolute = str(text_file_path_absolute)
@@ -43,34 +50,43 @@ def generate_srt(number, word):
     task.output_sync_map_file()
 
     # Loads the JSON data into a Python dictionary
-    with open(sync_map_file_path_absolute, 'r') as f:
+    with open(sync_map_file_path_absolute, "r") as f:
         data = json.load(f)
 
     # Helper function to convert time from seconds to SRT format
     def convert_time(time_string):
-        hours, minutes, seconds = int(float(time_string) // 3600), int(float(time_string) % 3600 // 60), float(time_string) % 60
-        return f"{hours:02}:{minutes:02}:{seconds:06.3f}".replace('.', ',')
+        hours, minutes, seconds = (
+            int(float(time_string) // 3600),
+            int(float(time_string) % 3600 // 60),
+            float(time_string) % 60,
+        )
+        return f"{hours:02}:{minutes:02}:{seconds:06.3f}".replace(".", ",")
 
     # Begin forming the SRT data
     srt_data = ""
 
-    for idx, fragment in enumerate(data['fragments'], start=1):
-        begin = convert_time(fragment['begin'])
-        end = convert_time(fragment['end'])
-        lines = "\n".join(fragment['lines'])
+    for idx, fragment in enumerate(data["fragments"], start=1):
+        begin = convert_time(fragment["begin"])
+        end = convert_time(fragment["end"])
+        lines = "\n".join(fragment["lines"])
         srt_data += f"{idx}\n{begin} --> {end}\n{lines}\n\n"
 
     # Optionally, save the SRT data to a file
-    with open(str(str_output), 'w') as file:
+    with open(str(str_output), "w") as file:
         file.write(srt_data)
 
     # Automatic download SRT file
     os.system(f"mv {str_output} {str(str_output)}")
 
-    print(f"SRT file generated and saved to {str_output}.")
+    print(f"[System]: SRT file generated and saved to {str_output}.")
+
 
 if __name__ == "__main__":
     # User input
+    print(
+        "[System]: Hello, We are going to make a subtitle(word by word) for with your audio file(consider modifying the input and output path of files).\nGive a 'number' and 'word' to create a file with these two!"
+    )
+    print("[System]: Sample output -> [number].[word].srt")
     number = input("Enter the number: ")
     word = input("Enter the word: ")
 
