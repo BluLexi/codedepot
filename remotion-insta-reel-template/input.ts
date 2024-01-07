97731da4651c3f9a4c621f2db76f5d044f397f50
Pouya <p.mohammadkhani95@gmail.com>
2024-01-07 21:13:09 +0330

    15

--- remotion-insta-reel-template/input.ts
+++ remotion-insta-reel-template/input.ts
@@ -1,10 +1,10 @@
 import { staticFile } from 'remotion';
 
 // ! Change inputs here
-const videoLength = 50;
+const videoLength = 54;
 const outroLength = 5;
-const number = 14
-const word = 'Negligible'
+const number = 15
+const word = 'Obey'
 const name = `${number}.${word}`
 const delay = 0.5
 // '#0a60af' '#5D3587' '#820300' '#3559E0' '#65B741'
